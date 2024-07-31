79d42ac9b1fd326a8b08da6d6a1b586df7ddb2ef
NO5feng <1226985060@qq.com>
2024-07-31 17:52:27 +0800

    [feat] MainActivity 未完成

--- app/src/main/java/com/example/wpj_kotlin/activity/MainActivity.kt
+++ app/src/main/java/com/example/wpj_kotlin/activity/MainActivity.kt
@@ -5,10 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import com.example.wpj_kotlin.activity.ui.theme.WPJ_KotlinTheme
 import com.example.wpj_kotlin.ui.MainUi
 
@@ -19,7 +16,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WPJ_KotlinTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
+                Scaffold() {
                     MainUi()
                 }
             }
--- app/src/main/java/com/example/wpj_kotlin/ui/MainScreen.kt
+++ app/src/main/java/com/example/wpj_kotlin/ui/MainScreen.kt
@@ -1,23 +1,39 @@
 package com.example.wpj_kotlin.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import com.example.wpj_kotlin.R
 import com.example.wpj_kotlin.activity.ui.theme.WPJ_KotlinTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainUi() {
+    val context = LocalContext.current
+    val backgroundColor = ContextCompat.getColor(context, R.color.yellow)
+    TopAppBar(
+        title = { Text(text = stringResource(id = R.string.home_title), color = Color.White) },
+
+    )
     Column(
-        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+            .background(Color(backgroundColor))
     ) {
         Text(
             text = "Hello Word",
